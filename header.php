04a894ee97a28c4a1f6e66e858a5dcef0f14055e
deansimcox <deansimcox@gmail.com>
2016-09-19 19:53:40 +0800

    added facebook graph tag

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta property="og:image" content="http://fusc.org.au/new/wp-content/themes/fusc-wp-theme/images/logo-fusc.svg" />
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
